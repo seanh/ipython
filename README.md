0520d74ec883bfad279aa0660597f7fed4eda6b5
Sean Hammond <seanh@users.noreply.github.com>
2023-03-19 12:51:07 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+IPython
+=======
+
+My [IPython](https://ipython.org/) config files.
+
+Installation
+------------
+
+```terminal
+git clone https://github.com/seanh/ipython.git ~/.ipython
+```
---
+++ profile_default/ipython_config.py
@@ -0,0 +1,5 @@
+c = get_config()
+c.TerminalIPythonApp.display_banner = False
+c.TerminalInteractiveShell.confirm_exit = False
+c.TerminalInteractiveShell.prompts_class = 'IPython.terminal.prompts.ClassicPrompts'
+c.TerminalInteractiveShell.separate_in = ''
