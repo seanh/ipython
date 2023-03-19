aad69321b3a05a3034cdfeb1a6ca384c81c9508c
Sean Hammond <seanh@users.noreply.github.com>
2023-03-19 12:59:27 +0000

    Support custom banners if one is set
    But just don't have any banner by default.

--- profile_default/ipython_config.py
+++ profile_default/ipython_config.py
@@ -1,5 +1,5 @@
 c = get_config()
-c.TerminalIPythonApp.display_banner = False
+c.TerminalInteractiveShell.banner1 = ""
 c.TerminalInteractiveShell.confirm_exit = False
-c.TerminalInteractiveShell.prompts_class = 'IPython.terminal.prompts.ClassicPrompts'
-c.TerminalInteractiveShell.separate_in = ''
+c.TerminalInteractiveShell.prompts_class = "IPython.terminal.prompts.ClassicPrompts"
+c.TerminalInteractiveShell.separate_in = ""
