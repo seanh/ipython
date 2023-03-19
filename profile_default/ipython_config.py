9e522f82305c6b73eff83ad481533923b964d671
Sean Hammond <seanh@users.noreply.github.com>
2023-03-19 13:04:07 +0000

    Show Python version in default startup banner

--- profile_default/ipython_config.py
+++ profile_default/ipython_config.py
@@ -1,5 +1,7 @@
+import sys
+
 c = get_config()
-c.TerminalInteractiveShell.banner1 = ""
+c.TerminalInteractiveShell.banner1 = f"Python {sys.version}\n"
 c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalInteractiveShell.prompts_class = "IPython.terminal.prompts.ClassicPrompts"
 c.TerminalInteractiveShell.separate_in = ""
